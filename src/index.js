8f454b9ec5587b9cfff4ac2bca1550afb9193193
Douglas Cavalcanti Teles dos Santos <44294260+ProxDouglas@users.noreply.github.com>
2022-06-29 14:20:48 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Olá, mundo')
+const mongoose = require('mongoose');
+const express = require('express');
+var app = express();
+// var session = require("express-session");
+var flash = require("express-flash");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+
+connection().catch(err => console.log(err));
+
+async function connection() {
+  await mongoose.connect('mongodb+srv://groupLobtec:groupLobtec@e-commerce.pjmhya7.mongodb.net/?retryWrites=true&w=majority');
+  console.log("Connect database")
+}
